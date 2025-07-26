e99e7643ce5ec3e6afd6a90c287dc570d8c005a6
Nickolas Bini <nickolasbini@hotmail.com>
2025-07-26 10:33:18 -0300

    Added JWT token

--- docker/init.sql
+++ docker/init.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL UNIQUE
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(255)
 );
 
-INSERT INTO users (nome, email) VALUES
+INSERT INTO users (name, email) VALUES
 ('Maria Silva', 'maria@example.com'),
 ('Pedro Santos', 'pedro@example.com'),
 ('Ana Oliveira', 'ana@example.com');
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const db = require('./db/db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 dotenv.config();
 
@@ -8,16 +10,80 @@ const app = express();
 const enviroment = process.env.ENVIROMENT ?? 'local';
 const port = process.env.PORT || 3000;
 const ip = process.env.IP || '';
+const hash_salt = parseInt(process.env.HASH_SALT) || 10;
+const jwtSecret = process.env.JWT_SECRET || '11kjsajg@#';
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        endpoints: ['/users']
-    })
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'] ?? '';
+    const token = authHeader.split(' ')[1] ?? null;
+    
+    if (!token) {
+        return res.status(401).json({ error: 'token not found' });
+    }
+
+    jwt.verify(token, jwtSecret, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'invalid token' });
+        }
+
+        req.authUser = user;
+        next();
+    });
+}
+
+
+app.post('/register', async (req, res) => {
+    const { email, password, name } = req.body;
+
+    try {
+        const hashedPass = await bcrypt.hash(password, hash_salt);
+        const [result] = await db.query('INSERT INTO users (email, password, name) VALUES (?, ?, ?)', [email, hashedPass, name]);
+        const userId = result.insertId ?? null;
+
+        if (!userId) {
+            return res.status(500).json({ message: 'Usuário não foi cadastrado!' });
+        }
+        res.status(201).json({ message: 'Usuário cadastrado', userId: userId });
+
+    } catch (error) {
+        console.log('error at register', error);
+        res.status(500).json({ message: 'Erro ao cadastrar o usuário' });
+    }
 });
 
-app.get('/users', async (req, res) => {
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const [results] = await db.execute('SELECT * FROM users WHERE email = ? LIMIT 1', [email]);
+        const user = results[0] ?? null;
+
+        if (!user) {
+            return res.status(401).json({ message: 'Email ou senha inválidos!' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Email ou senha inválidos!' });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email, name: user.name },
+            jwtSecret,
+            { expiresIn: '1h' }
+        );
+        res.json({ token });
+
+    } catch (error) {
+        console.log('error at login', error);
+        res.status(500).json({ message: 'Erro ao logar' });
+    }
+});
+
+app.get('/users', authenticateToken, async (req, res) => {
     try {
         const [users] = await db.execute('SELECT * FROM users');
         res.json(users);
@@ -27,7 +93,7 @@ app.get('/users', async (req, res) => {
     }
 });
 
-app.get('/users/:id', async (req, res) => {
+app.get('/users/:id', authenticateToken, async (req, res) => {
     try {
         const [users] = await db.execute(
             'SELECT * FROM users WHERE id = ? LIMIT 1',
